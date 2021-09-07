ff924a722424d1bced2e2c2662a5378021340b25
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-09-07 12:23:41 +0200

    added hevm addr constant to tests (#7)

--- src/test/utils/GreeterTest.sol
+++ src/test/utils/GreeterTest.sol
@@ -22,8 +22,7 @@ contract User {
 }
 
 contract GreeterTest is DSTest {
-    Hevm internal constant hevm =
-        Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    Hevm internal constant hevm = Hevm(HEVM_ADDRESS);
 
     // contracts
     Greeter internal greeter;
