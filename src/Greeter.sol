e60fa37e08ac84adaffcf8fd22e8107dee5b48d2
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-09-24 14:16:30 +1000

    feat: solhint

--- src/Greeter.sol
+++ src/Greeter.sol
@@ -4,20 +4,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 library Errors {
-    string constant InvalidBlockNumber = "invalid block number, please wait";
-    string constant CannotGm = "cannot greet with gm";
+    string internal constant InvalidBlockNumber =
+        "invalid block number, please wait";
+    string internal constant CannotGm = "cannot greet with gm";
 }
 
 contract Greeter is Ownable {
     string public greeting;
 
-    function gm() onlyOwner public {
+    function gm() public onlyOwner {
         require(block.number % 10 == 0, Errors.InvalidBlockNumber);
         greeting = "gm";
     }
 
     function greet(string memory _greeting) public {
-        require(keccak256(abi.encodePacked(_greeting)) != keccak256("gm"), Errors.CannotGm);
+        require(
+            keccak256(abi.encodePacked(_greeting)) != keccak256("gm"),
+            Errors.CannotGm
+        );
         greeting = _greeting;
     }
 }
