0b81a545fafad018a0e90a2dab0ae0772b051abf
Gautham Elango <gautham.gg@gmail.com>
2021-09-27 19:59:14 +1000

    feat: solhint + solhint-plugin-prettier for linting (#19)
    * feat: solhint
    
    * fix: add --fix to solhint script + add a lint check script
    
    * chore: simplify solhint script
    
    * chore: simplify prettier script

--- src/test/Greeter.t.sol
+++ src/test/Greeter.t.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "./utils/GreeterTest.sol";
-import { Errors } from "../Greeter.sol";
+import {Errors} from "../Greeter.sol";
 
 contract Greet is GreeterTest {
     function testCannotGm() public {
-        try alice.greet("gm") { fail(); } catch Error(string memory error) {
+        try alice.greet("gm") {
+            fail();
+        } catch Error(string memory error) {
             assertEq(error, Errors.CannotGm);
         }
     }
@@ -20,7 +22,6 @@ contract Greet is GreeterTest {
         alice.greet(greeting);
         assertEq(greeter.greeting(), greeting);
     }
-
 }
 
 contract Gm is GreeterTest {
@@ -32,15 +33,18 @@ contract Gm is GreeterTest {
 
     function testOwnerCannotGmOnBadBlocks() public {
         hevm.roll(11);
-        try alice.gm() { fail(); } catch Error(string memory error) {
+        try alice.gm() {
+            fail();
+        } catch Error(string memory error) {
             assertEq(error, Errors.InvalidBlockNumber);
         }
     }
 
     function testNonOwnerCannotGm() public {
-        try bob.gm() { fail(); } catch Error(string memory error) {
+        try bob.gm() {
+            fail();
+        } catch Error(string memory error) {
             assertEq(error, "Ownable: caller is not the owner");
         }
     }
 }
-
