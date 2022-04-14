a58cb963923a00547e6440bae31e222fab39150c
Georgios Konstantopoulos <me@gakonst.com>
2022-04-14 12:46:50 -0700

    refactor: forge-std + tests under separate folder

---
+++ test/Contract.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+contract TestContract is Test {
+    function setUp() public { }
+
+    function testFoo() public {
+        assertTrue(true);
+    }
+}
