018646256d9d1a56938eddca370564a06e74ae18
Cyrus of Eden <knouroozi@gmail.com>
2022-08-24 13:28:42 -0700

    Require caller to own the appraised NFT

--- src/appraisers/NiftyAppraiser.sol
+++ src/appraisers/NiftyAppraiser.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.13;
 
+import {ERC721} from "solmate/tokens/ERC721.sol";
+
 import {ERC20, SafeTransferLib} from "@omniprotocol/libraries/SafeTransferLib.sol";
 import {Pausable} from "@omniprotocol/mixins/Pausable.sol";
 import {Stewarded} from "@omniprotocol/mixins/Stewarded.sol";
@@ -9,9 +11,7 @@ import {EIP2612Storage} from "@contracts/mixins/EIP2612Storage.sol";
 
 contract NiftyAppraiser is EIP2612Storage, Pausable, Stewarded {
     using SafeTransferLib for ERC20;
-
     ERC20 public rewardToken;
-    mapping(bytes32 => uint256) public rewardOf;
 
     constructor(address _steward, address _rewardToken) {
         __initEIP2612("NiftyAppraiser", "1");
@@ -26,6 +26,8 @@ contract NiftyAppraiser is EIP2612Storage, Pausable, Stewarded {
         uint256 value
     );
 
+    mapping(bytes32 => uint256) public rewardOf;
+
     function claim(
         address signer,
         address nifty,
@@ -36,7 +38,9 @@ contract NiftyAppraiser is EIP2612Storage, Pausable, Stewarded {
         bytes32 r,
         bytes32 s
     ) public returns (bytes32 sighash) {
-        require(deadline >= block.timestamp, "DEADLINE_EXPIRED");
+        // Checks
+        require(block.timestamp <= deadline, "DEADLINE_EXPIRED");
+        require(msg.sender == ERC721(nifty).ownerOf(id), "NOT_YOUR_NIFTY");
 
         sighash = keccak256(
             abi.encodePacked(
@@ -56,19 +60,21 @@ contract NiftyAppraiser is EIP2612Storage, Pausable, Stewarded {
                 )
             )
         );
-        address recoveredAddress = ecrecover(sighash, v, r, s);
+        address authorizer = ecrecover(sighash, v, r, s);
 
         require(
-            recoveredAddress != address(0) &&
-                recoveredAddress == signer &&
-                isAuthorized(recoveredAddress, msg.sig),
+            authorizer != address(0) &&
+                authorizer == signer &&
+                isAuthorized(authorizer, msg.sig),
             "INVALID_SIGNER"
         );
         require(rewardOf[sighash] == 0, "INVALID_SIGNATURE");
 
+        // Effects
         rewardOf[sighash] = value;
         emit Appraisal(signer, nifty, id, value);
 
+        // Interactions
         rewardToken.safeTransfer(msg.sender, value);
     }
 }
